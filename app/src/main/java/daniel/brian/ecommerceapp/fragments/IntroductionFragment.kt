eaea577028ef6f351642de758e0606837b0b9aec
Daniel Brian <db9755949@gmail.com>
2023-10-12 20:11:24 +0300

    Introduction Fragment

---
+++ app/src/main/java/daniel/brian/ecommerceapp/fragments/IntroductionFragment.kt
@@ -0,0 +1,25 @@
+package daniel.brian.ecommerceapp.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import daniel.brian.ecommerceapp.R
+
+class IntroductionFragment : Fragment(R.layout.fragment_introduction) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // TODO
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_introduction, container, false)
+    }
+}
