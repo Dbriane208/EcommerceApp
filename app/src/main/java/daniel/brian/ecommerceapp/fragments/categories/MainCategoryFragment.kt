e339a9f136aacdca3fe6a036b28c6a00a6178ace
Daniel Brian <db9755949@gmail.com>
2024-01-26 11:29:59 +0300

    minor changes

--- app/src/main/java/daniel/brian/ecommerceapp/fragments/categories/MainCategoryFragment.kt
+++ app/src/main/java/daniel/brian/ecommerceapp/fragments/categories/MainCategoryFragment.kt
@@ -1,5 +1,20 @@
 package daniel.brian.ecommerceapp.fragments.categories
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import daniel.brian.ecommerceapp.databinding.FragmentMainCategoryBinding
 
-class MainCategoryFragment : Fragment()
+class MainCategoryFragment : Fragment(){
+    private lateinit var binding: FragmentMainCategoryBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainCategoryBinding.inflate(layoutInflater)
+        return binding.root
+    }
+}
